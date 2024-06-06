cf18c02d0f04c97fff9d542c06f3028153cc6c78
fausto-machava <fasthymachava12@gmail.com>
2024-06-06 23:45:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("header nav ul li a");
+    const sections = document.querySelectorAll("section");
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 100 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+    }
+
+    changeLinkState();
+    window.addEventListener("scroll", changeLinkState);
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
